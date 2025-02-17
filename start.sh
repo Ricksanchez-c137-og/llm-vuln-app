c7aa66a91cb50c808122ad9f603356b1405566a5
Ricksanchez-c137-og <sobermanisgod@gmail.com>
2025-02-17 18:50:25 +0400

    mods: start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
-
+echo "ADMIN ONLY"
 echo "Starting Ollama..."
 ollama serve &
-
-# Wait for Ollama to become available
 until curl -s http://127.0.0.1:11434/api/tags > /dev/null; do
   echo "Waiting for Ollama to start..."
   sleep 2
 done
-
 echo "Pulling AI models..."
 ollama pull mistral
 echo "Models loaded successfully!"
-
-# Start FastAPI & Next.js
 echo "Starting FastAPI and Next.js..."
 uvicorn app:app --host 0.0.0.0 --port 8000 & npm start
